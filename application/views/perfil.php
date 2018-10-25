7f3890f5e6585014b8e904d470032c78d6393f9d
MatheusLealAquino <matheusaquino@id.uff.br>
2018-10-25 19:38:55 -0300

    Change label when select image

--- application/views/perfil.php
+++ application/views/perfil.php
@@ -51,3 +51,10 @@
         </div>
     </div>
 </div>
+
+<script>
+$('.custom-file-input').on('change', function() { 
+   let fileName = $(this).val().split('\\').pop(); 
+   $(this).next('.custom-file-label').addClass("selected").html(fileName); 
+});
+</script>
